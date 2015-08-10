95f686897a3a912e81dbc8b4f5b3fc0e08bf80e1
dignifiedquire <dignifiedquire@gmail.com>
2015-08-10 21:20:29 +0200

    chore: Update devDependencies

--- gruntfile.js
+++ gruntfile.js
@@ -6,6 +6,29 @@ module.exports = function (grunt) {
         commitMessage: 'chore: update contributors'
       }
     },
+    conventionalChangelog: {
+      release: {
+        options: {
+          changelogOpts: {
+            preset: 'angular'
+          }
+        },
+        src: 'CHANGELOG.md'
+      }
+    },
+    conventionalGithubReleaser: {
+      release: {
+        options: {
+          auth: {
+            type: 'oauth',
+            token: process.env.GH_TOKEN
+          },
+          changelogOpts: {
+            preset: 'angular'
+          }
+        }
+      }
+    },
     bump: {
       options: {
         commitMessage: 'chore: release v%VERSION%',
@@ -32,8 +55,9 @@ module.exports = function (grunt) {
     grunt.task.run([
       'npm-contributors',
       'bump-only:' + (type || 'patch'),
-      'changelog',
+      'conventionalChangelog',
       'bump-commit',
+      'conventionalGithubReleaser',
       'npm-publish'
     ])
   })
